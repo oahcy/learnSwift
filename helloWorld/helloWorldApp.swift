8f4ad9a2e623e2b32959402adb895a441a720eff
oahcy <1162485779@qq.com>
2022-12-02 15:29:54 +0800

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by csy on 2022/12/2.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
